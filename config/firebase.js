28256863a410c517963f8138f4ba3a6abc539520
Sanjaykumar-rengaraju <sanjaykumarrengaraju@gmail.com>
2025-04-12 18:26:14 +0530

    Production Ready

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,7 @@
-require("dotenv").config(); // Load environment variables from .env
+require("dotenv").config();
 const admin = require("firebase-admin");
-const fs = require("fs");
-const path = require("path"); // Include path module
 
-// Get Firebase config from environment variable and resolve it correctly
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, process.env.FIREBASE_CONFIG_PATH), "utf-8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
