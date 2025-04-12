e3a3c78ad6813e175c16aeb430ed570444a7845d
Sanjaykumar-rengaraju <sanjaykumarrengaraju@gmail.com>
2025-04-12 18:30:04 +0530

    Production Ready

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is not defined");
+}
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
